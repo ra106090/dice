69271df3f7229ca3d75d6607f0f8f31bb61a89a7
ra106090 <112607026+ra106090@users.noreply.github.com>
2022-09-06 13:29:46 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    number = 0
+})
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(0, 6))
+    basic.showNumber(randint(1, 6))
 })
